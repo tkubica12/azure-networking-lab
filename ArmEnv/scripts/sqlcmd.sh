89536b9a490c26063c40b04fb6e4aeb87c8e7266
Tomas Kubica <tkubica12@gmail.com>
2018-12-18 10:00:43 +0100

    vpn reference api + nginx script

--- ArmEnv/scripts/sqlcmd.sh
+++ ArmEnv/scripts/sqlcmd.sh
@@ -3,4 +3,4 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update 
 sudo ACCEPT_EULA=Y apt-get install mssql-tools unixodbc-dev -y
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /home/$(awk -F: '{ print $1}' /etc/passwd | tail -n 1)/.bashrc
