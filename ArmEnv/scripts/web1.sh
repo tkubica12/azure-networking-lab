a89d7a4ff278feb6c6bcdf0bee6062182329f0dc
Tomas Kubica <tkubica12@gmail.com>
2019-03-22 20:46:58 +0100

    webFix

--- ArmEnv/scripts/web1.sh
+++ ArmEnv/scripts/web1.sh
@@ -2,4 +2,4 @@
 runcmd:
     - sudo apt update 
     - sudo apt install apache2 -y
-    - echo "WEB: Server 1 is alive!" | sudo tee /var/www/html/index.html
+    - echo "WEB Server 1 is alive!" | sudo tee /var/www/html/index.html
--- ArmEnv/scripts/web2.sh
+++ ArmEnv/scripts/web2.sh
@@ -2,4 +2,4 @@
 runcmd:
     - sudo apt update 
     - sudo apt install apache2 -y
-    - echo "WEB: Server 2 is alive!" | sudo tee /var/www/html/index.html
+    - echo "WEB Server 2 is alive!" | sudo tee /var/www/html/index.html
